d171df31298f53604dbe4afcfca44db968287313
Unknown <jrj8819@koreatech.ac.kr>
2018-12-11 04:22:43 +0900

    뷰플리퍼 예시 코드 작성

---
+++ example/ViewPager/app/src/main/java/rj/viewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package rj.viewpager;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rj.viewpager.adapters.CustomPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+    }
+}
