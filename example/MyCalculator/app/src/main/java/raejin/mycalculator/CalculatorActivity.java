114c5dd7cf15186c6a32503b15ecfb87144d7947
raejin <jrj8819@koreatech.ac.kr>
2018-12-11 13:06:00 +0900

    MyCalculator 예제 추가

--- example/MyCalculator/app/src/main/java/raejin/mycalculator/CalculatorActivity.java
+++ example/MyCalculator/app/src/main/java/raejin/mycalculator/CalculatorActivity.java
@@ -1,14 +1,13 @@
-package rj.helloworld;
+package raejin.mycalculator;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class CalculatorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
