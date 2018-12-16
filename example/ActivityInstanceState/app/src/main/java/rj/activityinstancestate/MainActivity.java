733ece3c16fb856591be8eeaeb953d344ac44407
Unknown <jrj8819@koreatech.ac.kr>
2018-12-16 23:28:34 +0900

    life cycle 예제 추가

---
+++ example/ActivityInstanceState/app/src/main/java/rj/activityinstancestate/MainActivity.java
@@ -0,0 +1,50 @@
+package rj.activityinstancestate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv_count;
+    Button btn_add;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_count = (TextView)findViewById(R.id.tv_count);
+        btn_add = (Button)findViewById(R.id.btn_add);
+
+        tv_count.setText(String.valueOf(count));
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                tv_count.setText(String.valueOf(count));
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("temp_data", count);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("temp_data");
+        tv_count.setText(String.valueOf(count));
+    }
+}
